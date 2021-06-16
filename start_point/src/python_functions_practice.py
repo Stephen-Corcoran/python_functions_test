91fd635d012179bb5f58b058671ed6cdbe23ba99
Stephen <stephendcorcoran@gmail.com>
2021-06-16 16:45:41 +0100

    Initial commit after doing most of the tasks opps

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,32 @@
+def return_10():
+    return 10
+
+def add(num1,num2):
+    return (num1 + num2)
+
+def subtract (num1, num2):
+    return (num1 - num2)
+
+def multiply (num1, num2):
+    return (num1 * num2)
+
+def divide (num1, num2):
+    return (num1 / num2)
+    
+def length_of_string (string):
+    return len(string)
+
+def join_string (string_1, string_2):
+    return (string_1 + string_2)
+
+def add_string_as_number (num1, num2):
+    return int(num1)+ int(num2)
+
+def number_to_full_month_name (months_names):
+    if months_names == 1:
+        return "January"
+    
+
+
+
+
